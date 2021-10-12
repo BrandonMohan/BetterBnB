d9e76be1cddd92bad2cd991e91fece7bfb30f818
BrandonMohan <barndonmo@gmail.com>
2021-10-12 08:51:32 -0400

    mini push

--- backend/db/models/spot.js
+++ backend/db/models/spot.js
@@ -11,7 +11,8 @@ module.exports = (sequelize, DataTypes) => {
   }, {});
   Spot.associate = function(models) {
     // associations can be defined here
-    
+    Spot.belongsTo(models.User, {foreignKey: 'userId'})
+    Spot.hasMany(models.Review, {foreignKey: 'spotId'})
   };
   return Spot;
 };
