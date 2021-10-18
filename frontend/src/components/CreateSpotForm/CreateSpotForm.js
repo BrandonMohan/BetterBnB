b73e42f317400357d6bd4741d0b0dcc2697f010c
BrandonMohan <barndonmo@gmail.com>
2021-10-18 00:46:07 -0400

    fixed modals

--- frontend/src/components/CreateSpotForm/CreateSpotForm.js
+++ frontend/src/components/CreateSpotForm/CreateSpotForm.js
@@ -50,42 +50,57 @@ const CreateSpotForm = () => {
     return (
         <section className="new-spot-form">
             <form onSubmit={handleSubmit}>
+                <fieldset>
+                <legend>Create a Spot</legend>
+                <div>
                 <input
                 type='text'
                 placeholder='Address'
                 value={address}
                 onChange={updateAddress}
                 />
+                </div>
+                <div>
                 <input
                 type='text'
                 placeholder='City'
                 value={city}
                 onChange={updateCity}
                 />
+                </div>
+                <div>
                 <input
                 type='text'
                 placeholder='State'
                 value={state}
                 onChange={updateState}
                 />
+                </div>
+                <div>
                 <input
                 type='text'
                 placeholder='Country'
                 value={country}
                 onChange={updateCountry}
                 />
+                </div>
+                <div>
                 <input
                 type='text'
                 placeholder='Name'
                 value={name}
                 onChange={updateName}
                 />
+                </div>
+                <div>
                 <input
                 type='text'
                 placeholder='Price'
                 value={price}
                 onChange={updatePrice}
                 />
+                </div>
+                <div>
                 <input
                 type='text'
                 placeholder='image-url'
@@ -93,7 +108,9 @@ const CreateSpotForm = () => {
                 onChange={updateImageUrl}
                 required
                 />
+                </div>
                 <button className='create_spot_button' type='submit'>Create new Spot</button>
+                </fieldset>
             </form>
         </section>
     );
